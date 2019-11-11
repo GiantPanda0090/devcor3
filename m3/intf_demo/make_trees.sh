823e5a7f698c1d7f81998bd9ea9a461c708a8649
nick <njrusmc@Gmail.com>
2019-11-11 17:31:28 -0500

    m3 pre demo

--- m3/intf_demo/make_trees.sh
+++ m3/intf_demo/make_trees.sh
@@ -2,11 +2,11 @@
 # Small script to avoid typing these long commands
 
 # Build tree for IETF basic interfaces
-pyang --format tree --path ../yang_big \
+pyang --format tree --path ../../yang_big \
   --output data_ref/ietf_intf.txt \
-  ../yang_big/standard/ietf/RFC/ietf-interfaces.yang
+  ../../yang_big/standard/ietf/RFC/ietf-interfaces.yang
 
 # Build tree for IETF interface IP-specific components
-pyang --format tree --path ../yang_big \
+pyang --format tree --path ../../yang_big \
   --output data_ref/ietf_ip.txt \
-  ../yang_big/standard/ietf/RFC/ietf-ip.yang
+  ../../yang_big/standard/ietf/RFC/ietf-ip.yang

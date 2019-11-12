87ff32877e42cd5fe6d6e5b747be94aa0b11a951
nick <njrusmc@Gmail.com>
2019-11-11 21:48:29 -0500

    m3 vlan pre demo

--- m3/vlan_demo/make_trees.sh
+++ m3/vlan_demo/make_trees.sh
@@ -2,6 +2,6 @@
 # Small script to avoid typing these long commands
 
 # Build tree for Cisco-modeled VLAN-specific components
-pyang --format tree --path ../yang_big --lax-quote-checks \
+pyang --format tree --path ../../yang_big --lax-quote-checks \
   --output data_ref/xe16121_vlan.txt \
-  ../yang_big/vendor/cisco/xe/16121/Cisco-IOS-XE-vlan.yang
+  ../../yang_big/vendor/cisco/xe/16121/Cisco-IOS-XE-vlan.yang
